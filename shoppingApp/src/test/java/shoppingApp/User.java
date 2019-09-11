d514cabb653fa59d4bf15d3967294da48b26800b
Gayan Sampath Manamendara <gayan_m@epic.lk>
2019-09-11 09:52:48 +0530

    add junit

---
+++ shoppingApp/src/test/java/shoppingApp/User.java
@@ -0,0 +1,14 @@
+package shoppingApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class User {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
